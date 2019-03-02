369946fb74a14c0aca739eb880d067b2ede58c6b
Nathan Gazey <ngazey60@maine.edu>
2019-03-02 15:39:57 -0500

    added some serial data reading

--- relayData.py
+++ relayData.py
@@ -1,9 +1,12 @@
 import requests
 import time
+import serial
+
 
 
 url = 'http://url.com'
-waitTime = 5
+waitTime = 5                                     # In seconds
+
 
 
 def sendData(value):
@@ -11,12 +14,26 @@ def sendData(value):
     res = requests.post(url, data=query)
     return res.text
 
+
+
+
 def main():
+
+    serialInput = serial.Serial('/dev/ttyACM0')  # This should be modified to accept data from serial input
+
+    serialInput.flush()
+
     while True:
         # Wait some given time
         time.sleep(waitTime)
 
         # Collect data from sensors
+        try:
+            ser_bytes = serialInput.readline()
+            decoded_bytes = float(ser_bytes[0:len(ser_bytes) - 2].decode("utf-8"))
+            # Send data along to server
+            sendData(decoded_bytes)
 
-        # Send data along to server
-        sendData()
+        except:
+            print("Keyboard Interrupt")
+            break
