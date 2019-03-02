78d92180d996220cba3147967f659cf271ab4346
Nathan Gazey <ngazey60@maine.edu>
2019-03-02 15:52:38 -0500

    abstracted into a group of sensors sending out

--- relayData.py
+++ relayData.py
@@ -14,7 +14,6 @@ def sendData(value):
     res = requests.post(url, data=query)
     return res.text
 
-
 def serRead(serialinput1):
         ser_bytes = serialinput1.readline()
         decoded_bytes = float(ser_bytes[0:len(ser_bytes) - 2].decode("utf-8"))
@@ -22,17 +21,24 @@ def serRead(serialinput1):
         # Send data along to server
         sendData(decoded_bytes)
 
-
 def main():
-    serialinput1 = serial.Serial('/dev/ttyACM0')  # This should be modified to accept data from serial input
-    serialinput1.flush()
+    serialinput0 = serial.Serial('/dev/ttyACM0')
+    serialinput1 = serial.Serial('/dev/ttyACM1')
+    serialinput2 = serial.Serial('/dev/ttyACM2')
+    serialinput3 = serial.Serial('/dev/ttyACM3')
+
+    serialinputs = [serialinput0, serialinput1, serialinput2, serialinput3]
+
     while True:
         # Wait some given time
         time.sleep(waitTime)
 
         # Collect data from sensors
         try:
-            serRead(serialinput1)
+            for serinput in serialinputs:
+                serRead(serinput)
         except KeyboardInterrupt:
             print("Keyboard Interrupt")
             break
+
+   
