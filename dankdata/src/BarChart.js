36ec971f9c47f4db2cd2331ab3d70ea9608d75f8
Nathan Gazey <ngazey60@maine.edu>
2019-03-02 19:46:01 -0500

    added a base style to our site

---
+++ dankdata/src/BarChart.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import * as d3 from "d3";
+import './BarChart.css'
+
+class BarChart extends Component {
+    render() {
+        return (
+            <div className="BarChart">
+                im a bar graph.
+            </div>
+        );
+    }
+
+}
+
+export default BarChart;
